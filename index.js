02edd261fb72ba15806ebe97d31b97ee7bf48d3b
Prem Kumar <prem.kumar@gmail.com>
2021-12-14 23:07:35 +0530

    Routing request by id

--- index.js
+++ index.js
@@ -19,8 +19,13 @@ app.set('view - engine', 'ejs');
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({});
-    console.log(products);
+    // console.log(products);
     res.render('products/index.ejs', { products });
+})
+app.get('/products/:id', async (req, res) => {
+    const { id } = req.params;
+    const foundProducts = await Product.findById(id);
+    res.render('products/show.ejs', { foundProducts });
 })
 
 app.listen(3000, () => {
